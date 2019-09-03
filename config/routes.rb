628507305f92348ced8ca4811dc50f4b1d0149ef
BranLiang <lby89757@hotmail.com>
2019-09-03 22:20:20 +0800

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+
+  # GraphQL API
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-
-  root to: "pages#home"
+  
+  # Authentication
+  get "/login", to: "auth#login"
 end
