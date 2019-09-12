e8e9b199bab227bd5a707dad5a44e02582ef73cb
BranLiang <lby89757@hotmail.com>
2019-09-12 20:02:48 +0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+bran = User.create(
+  name: 'Bran',
+  email: 'bran@corran.cn',
+  password: '654321',
+  password_confirmation: '654321'
+)
+
+# Clients
+twitter = Client.create(
+  name: 'Twitter'
+)
+
+# Vendors
+tencent = Vendor.create(
+  name: 'Tencent'
+)
+
+# Campaigns
+twitter_campaign = Campaign.create(
+  name: 'Twitter_Android_CN_CPA',
+  client: twitter
+)
+
+# Tracks
+Track.create(
+  url: 'http://www.tencent.com?track_id={TRACK_ID}',
+  campaign: twitter_campaign,
+  client: twitter
+)
+
