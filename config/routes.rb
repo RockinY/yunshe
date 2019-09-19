9924b2dbe6c572a8a1d4b7cb24066640810dc7c7
BranLiang <lby89757@hotmail.com>
2019-09-19 23:42:13 +0800

    Setup client index page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout' }
   
   # Pages
-  resources :clients, only: [:index, :new, :create]
+  resources :clients, only: [:index, :new, :create, :show]
   resources :vendors, only: [:index]
 end
